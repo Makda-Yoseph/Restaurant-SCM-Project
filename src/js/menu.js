64e3a131be53ae8dc51356b5ee133213a448db51
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2025-12-24 23:25:51 +0300

    Format price to two decimal places in menu display

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,7 +2,7 @@ fetch("data/menu.json")
 .then(res => res.json())
 .then(items => {
     const menuDiv = document.getElementById("menu");
-    menuDiv.innerHTML = items.map(i => `<p>${i.name} - $${i.price}</p>`).join("");
+    menuDiv.innerHTML = items.map(i => `<p>${i.name} - $${i.price.toFixed(2)}</p>`).join("");
 });
 
 function orderItem(itemName) {
